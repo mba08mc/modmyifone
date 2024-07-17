54127b9743010de0a74015015e9fec34137870a8
Bilal <88000853+mba08mc@users.noreply.github.com>
2024-07-17 13:33:57 +0100

    Create iphonemodem.php

---
+++ info/iphonemodem.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html dir="ltr" lang="en">
+<head>
+
+<meta name="viewport" content="width=320px; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+<link title="style" type="text/css" rel="stylesheet" href="/web/20090403135452cs_/http://modmyi.com/info/info.css">
+</head>
+<body>
+
+<!-- AJEnterprise request
+Site: ModMyiPhone
+Pool: MMi-320x45
+Type: 320x45 -->
+<iframe width="320" height="45" noresize scrolling="No" frameborder="0" marginheight="0" marginwidth="0" src="https://web.archive.org/web/20090403135452if_/http://adserver1.backbeatmedia.com/servlet/ajrotator/14155/0/viewHTML?pool=16044&amp;type=16042&amp;zone=5000"> <script language="JavaScript" src="https://web.archive.org/web/20090403135452js_/http://adserver1.backbeatmedia.com/servlet/ajrotator/14155/0/viewJScript?pool=16044&amp;type=16042&amp;zone=5000"></script> </iframe>
+<center><a href="https://web.archive.org/web/20090403135452/http://www.modmyi.com/forums/index.php?styleid=31"><img style="padding-top:10px;" src="/web/20090403135452im_/http://modmyi.com/info/d/new-info-header.png" border="0"></a></center>
+
+
+<h3>iPhoneModem by Addition</h3>
+<p>iPhoneModem, by Addition, is an application for the iPhone that aims at making it really easy to use the iPhone's Internet connection on your computer. Scroll down for more info/screenies.
+<br/>
+<center>
+<script type="text/javascript"><!--
+google_ad_client = "pub-9273532765860762";
+/* more info 300x250, created 3/10/08 */
+google_ad_slot = "1283137392";
+google_ad_width = 300;
+google_ad_height = 250;
+//-->
+</script>
+<script type="text/javascript" src="https://web.archive.org/web/20090403135452js_/http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+</center>
+</p>
+<p>iPhoneModem is available for installation on jailbroken devices using Cydia or Installer package managers; to make connecting a one click task, you should also download the helper application available at <a href="https://web.archive.org/web/20090403135452/http://www.iphonemodem.com/">www.iphonemodem.com</a>.</p>
+<br/>
+<img src="/web/20090403135452im_/http://modmyi.com/info/images/iphonemodem.png"/>
+<br/>
+<img src="/web/20090403135452im_/http://modmyi.com/info/images/iphonemodem1.png"/>
+</p>
+
+ 
+<script src="https://web.archive.org/web/20090403135452js_/http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-585951-3";
+urchinTracker();
+</script>
+<!-- Start Quantcast tag -->
+<script type="text/javascript" src="https://web.archive.org/web/20090403135452js_/http://edge.quantserve.com/quant.js"></script>
+<script type="text/javascript">
+_qacct="p-4bEZTZBVa48UQ";quantserve();</script>
+<noscript>
+<a href="https://web.archive.org/web/20090403135452/http://www.quantcast.com/p-4bEZTZBVa48UQ" target="_blank"><img src="https://web.archive.org/web/20090403135452im_/http://pixel.quantserve.com/pixel/p-4bEZTZBVa48UQ.gif" style="display: none" border="0" height="1" width="1" alt="Quantcast"/></a></noscript>
+<!-- End Quantcast tag -->
+</body>
+</html>
